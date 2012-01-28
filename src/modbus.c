c7782b3502100ac5516414e5dd37047bf9d16542
Stefan Nickl <Stefan.Nickl@gmail.com>
2012-01-28 19:35:45 +0100

    Change 32bit reordering for little endian
    Trying to read a 32 bit (float) value from a certain PLC via modbus yielded the words switched. This fixes the problem in my case, but configurability may be needed since the modbus protocol doesn't define how 32 bit values should be ordered.

--- src/modbus.c
+++ src/modbus.c
@@ -260,15 +260,13 @@ static float mb_register_to_float (uint16_t hi, uint16_t lo) /* {{{ */
   union
   {
     uint8_t b[4];
+    uint16_t s[2];
     float f;
   } conv;
 
 #if BYTE_ORDER == LITTLE_ENDIAN
-  /* little endian */
-  conv.b[0] = lo & 0x00ff;
-  conv.b[1] = (lo >> 8) & 0x00ff;
-  conv.b[2] = hi & 0x00ff;
-  conv.b[3] = (hi >> 8) & 0x00ff;
+  conv.s[0] = hi;
+  conv.s[1] = lo;
 #else
   conv.b[3] = lo & 0x00ff;
   conv.b[2] = (lo >> 8) & 0x00ff;
